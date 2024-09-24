fd4b43d91b7b51d5777d11ce5b864d9ad9ef3f0a
Vlagimir Ivanov <volnyivolodya@gmail.com>
2024-09-24 14:58:02 +0300

    Update urls.py

--- warehouse/urls.py
+++ warehouse/urls.py
@@ -14,13 +14,3 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
-from rest_framework.authtoken import views
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include("api.urls")),
-    path('api-auth/', include('rest_framework.urls')),
-    path('api-token-aut/', views.obtain_auth_token)
-]
