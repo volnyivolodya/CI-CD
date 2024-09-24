a6b1d1fdf64c4216fb1ffcd925c691a876887de4
Vlagimir Ivanov <volnyivolodya@gmail.com>
2024-09-24 14:55:44 +0300

    Update asgi.py

--- warehouse/asgi.py
+++ warehouse/asgi.py
@@ -7,10 +7,4 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 """
 
-import os
 
-from django.core.asgi import get_asgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'warehouse.settings')
-
-application = get_asgi_application()
